a100bdb671a859771d66e982dc3ee17e3280b761
Atravieso23 <agustintravieso@gmail.com>
2022-06-14 21:02:26 -0300

    funcion pagar y validacion numeros negativos + alertas

--- main.js
+++ main.js
@@ -9,10 +9,16 @@ function Capturar(){
     
     let nombreCapturar = document.getElementById("nombre").value;
     let gastoCapturar = document.getElementById("gasto").value;
-    if (nombreCapturar != "" && gastoCapturar != "" ){ 
+    
+    if (nombreCapturar == "" || gastoCapturar=="" ){ alert("Rellene ambos campos")} 
+    else if (gastoCapturar<0){alert("Ingresar valores positivos")}
+    
+    if (nombreCapturar != "" && gastoCapturar != "" && gastoCapturar>=0){ 
         nuevaPersona = new Persona(nombreCapturar,gastoCapturar);
         agregar();
-    }   
+        pagar();
+    }
+     
 }
 
 let Datos =[];
@@ -23,14 +29,16 @@ let Pagar=0
 function agregar(){
     
     Datos.push(nuevaPersona);
-    console.log(Datos)
-    document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevaPersona.nombre+'</td><td>'+ "$" + nuevaPersona.gasto+'</td></tbody> '
+    document.getElementById("tabla").innerHTML += '<tbody><td>'+nuevaPersona.nombre+'</td><td>'+ "$" + nuevaPersona.gasto+'</td></tbody> '   
+}
+
+function pagar(){
     Total+=Number(nuevaPersona.gasto)
     Personas+=1
     Pagar=Total/Personas
     document.getElementById("Total").innerHTML= "Total: $" + Total 
     document.getElementById("Pagar").innerHTML= "A cada uno le toca aportar: $ " + Pagar.toFixed(2) 
 
-}
+}   
 
 
